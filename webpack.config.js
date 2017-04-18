3035c32bc502517240f0ce98ca2cc02282805014
Vincent <vkammerer@gmail.com>
2017-04-18 12:38:18 -0400

    Replaces tabs with spaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 const webpack = require('webpack');
 
 module.exports = {
-	devtool: "cheap-module-source-map",
-	plugins: [
-		new webpack.EnvironmentPlugin({
-			NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
-		})
-	],
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	entry: {
-		'main': './src/index.jsx',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: [{
-					loader: 'babel-loader',
-				}],
-			},
-		]
-	}
+  devtool: "cheap-module-source-map",
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: {
+    'main': './src/index.jsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+        }],
+      },
+    ]
+  }
 };
